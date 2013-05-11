f356554a2dc551b0591f9516da3a25cf29df1bac
jsaenz <jsaenz@cs.nmsu.edu>
2013-05-10 21:58:31 -0700

    change email addy

--- send_form_email.php
+++ send_form_email.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
      
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    $email_to = "saenzrugby@gmail.com";
+    $email_subject = "test email";
      
      
     function died($error) {

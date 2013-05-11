7c0867d696994e346a01146ff7a6e56554e6a02f
jsaenz <jsaenz@cs.nmsu.edu>
2013-05-10 20:30:54 -0700

    Show more variables

--- process.php
+++ process.php
@@ -3,8 +3,13 @@
 
 Welcome 
 <?php 
-	echo $_GET["First_Name"]; 
+	echo $_GET["First_Name"];
+	echo " "; 
 	echo $_GET["Last_Name"];
+	echo "\n";
+	echo "Phone:" $_GET["Phone_Fax"];
+	echo "\n";
+
 ?>
 
 !<br>
